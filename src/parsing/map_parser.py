aa40f8f7cf66ae06875c99d283e5bd5864d7e890
Rémi Oudin <oudin@crans.org>
2016-11-07 22:55:41 +0100

    This section handles cell.xml file, which defins all possible cell types
    This script comes with a test script, and a file example. The cell.xml
    file is supposed to have many cell types different, and this is handled
    by the iterator in map_parser.py in method parse_all_cells(cell_xml)

--- src/parsing/map_parser.py
+++ src/parsing/map_parser.py
@@ -12,22 +12,41 @@ def parse_cell(cell_object):
     Parses a CellType attribute.
     """
     name = cell_object.attrib['name']
+    answer = {name : []}
     cell_params = cell_object.find('Params')
-    altitude = int(cell_params.find('altitude').text)
+    _altitude = cell_params.find('altitude')
+    if _altitude:
+        altitude = int(_altitude.text)
+        answer = {name: [altitude,]}
     picture = cell_params.find('picture').text
-    return {name : (altitude, picture)}
+    answer[name] += [picture]
+    _entities = cell_object.find("Entities")
+    if _entities:
+        entities = _entities.findall("Entity")
+        answer[name] += entities
+    return answer
+
+def parse_all_cells(cell_xml):
+    root = ET.parse(cell_xml)
+    cells = root.findall("CellType")
+    dict_cells = {}
+    for cell in cells:
+        dict_cells.update(parse_cell(cell))
+    return dict_cells
 
 def map_parser(map_xml):
     """
     The main parser for the map xml file.
     """
     root = ET.parse(map_xml)
-    map_characteristics = get_size(root.find('Params'))
-    print("Map characteristics: " + str(map_characteristics))
+    answer = {}
+    map_size = get_size(root.find('Params'))
+    answer.update({'size': map_size})
     available_cells = {}
     for cell in root.findall('CellType'):
         available_cells = { **available_cells, **parse_cell(cell) }
-    print(available_cells)
+    answer.update({'default cells': available_cells})
+    return answer
 
 
 def get_size(tree):
--- test/parsing/test_parser.py
+++ test/parsing/test_parser.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
 sys.path.append('../../src/parsing/')
+
 import map_parser
 
+def formatter(l):
+    """ Formats the output, to check tests """
+    for (key,item) in l.items():
+        print(" * Result: %s -> %s" % (key, item))
+
 def test_map_parser():
-    map_parser.map_parser("maps/map.xml")
+    map_parsed = map_parser.map_parser("maps/map.xml")
+    formatter(map_parsed)
+
+
+def test_cell_parser():
+    cells_parsed = map_parser.parse_all_cells("maps/cell.xml")
+    formatter(cells_parsed)
 
 
 if __name__=="__main__":
     test_map_parser()
+    test_cell_parser()

d1fe9713475a48d419537ffd04bd1c50d00b4860
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-11-10 15:43:22 +0100

    on tente de compiler le tex avec travis

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -15,22 +15,9 @@ __AUTHOR__="Jeroen de Bruijn"
 # Required global variables:
 # - TRAVIS_BUILD_NUMBER : The number of the current build.
 # - TRAVIS_COMMIT       : The commit that the current build is testing.
-# - DOXYFILE            : The Doxygen configuration file.
 # - GH_REPO_NAME        : The name of the repository.
 # - GH_REPO_REF         : The GitHub reference to the repository.
 # - GH_REPO_TOKEN       : Secure token to the github repository.
-#
-# For information on how to encrypt variables for Travis CI please go to
-# https://docs.travis-ci.com/user/environment-variables/#Encrypted-Variables
-# or https://gist.github.com/vidavidorra/7ed6166a46c537d3cbd2
-# For information on how to create a clean gh-pages branch from the master
-# branch, please go to https://gist.github.com/vidavidorra/846a2fc7dd51f4fe56a0
-#
-# This script will generate Doxygen documentation and push the documentation to
-# the gh-pages branch of a repository specified by GH_REPO_REF.
-# Before this script is used there should already be a gh-pages branch in the
-# repository.
-#
 ################################################################################
 
 ################################################################################
@@ -43,7 +30,7 @@ set -e
 mkdir code_docs
 cd code_docs
 
-# Get the current gh-pages branch
+# Get the current master branch
 echo "GIT CLONE TRAVIS"
 echo ${GH_REPO_REF}
 git clone -b travis "https://github.com/mkRPGDev/mkRPG.git/"
@@ -61,19 +48,19 @@ git config user.email "travis@travis-ci.org"
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
-
 doxygen Doxyfile
-echo 'DOXYGEN TEST DOXYFILE'
-ls
+
 #On le renomme pour préparer au changement de branche
 mv "doc" "doc_new"
+#On change de branche
 git checkout gh-pages
-ls -a
+#On écrase l'ancienne documentation
 rm -r doc
-ls -a
 mv "doc_new" "doc"
 
-ls -a
+#On compile le tex
+cd doc/latex
+make
 
 
 ################################################################################
@@ -89,16 +76,16 @@ echo 'Uploading documentation to the gh-pages branch...'
 # GitHub is smart enough to know which files have changed and which files have
 # stayed the same and will only update the changed files.
 git add --all
-echo "git add passed"
+echo "Status : git add has passed"
 
 # Commit the added files with a title and description containing the Travis CI
 # build number and the GitHub commit reference that issued this build.
 git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-echo "git commit passed"
+echo "Status : git commit has passed"
 
 # Force push to the remote gh-pages branch.
 # The ouput is redirected to /dev/null to hide any sensitive credential data
 # that might otherwise be exposed.
 
 git push -v --force "https://${GH_REPO_TOKEN}@github.com/mkRPGDev/mkRPG.git/"
-echo "git push passed"
+echo "Status : git push has passed"

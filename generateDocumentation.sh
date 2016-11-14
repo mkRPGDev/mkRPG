20ca571ddaa79cf0ad6158a5f15753f9f5f88fe1
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-11-14 19:57:38 +0100

    ça marche pas...

--- generateDocumentation.sh
+++ generateDocumentation.sh
@@ -59,16 +59,18 @@ doxygen Doxyfile
 
 #Si on se trouve dans master, on génère la doc dans gh-pages sinon dans la
 #branche courante
-if [ ${branch_name} = "travis" ]; then
-  echo 'We are in the master branch, generating in gh-pages'
-  #On le renomme pour préparer au changement de branche
-  mv "doc" "doc_new"
-  #On change de branche si on est dans master
-  git checkout gh-pages
-  #On écrase l'ancienne documentation
-  rm -r doc
-  mv "doc_new" "doc"
-fi
+#if [ ${branch_name} == "travis" ]; then
+
+echo 'We are in the master branch, generating in gh-pages'
+#On le renomme pour préparer au changement de branche
+mv "doc" "doc_new"
+#On change de branche si on est dans master
+git checkout gh-pages
+#On écrase l'ancienne documentation
+rm -r doc
+mv "doc_new" "doc"
+
+#fi
 
 #On compile le tex
 echo 'Compiling tex documentation ... '

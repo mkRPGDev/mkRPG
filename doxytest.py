4db0357ac6123dfacc4fb7df5d81aca59d7c5e5c
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-11-15 15:37:08 +0100

    consignes de documentation

---
+++ doxytest.py
@@ -0,0 +1,29 @@
+"""Documentation for this module.
+
+More details.
+"""
+# just use the # for small comments (it will not appear in the doc)
+
+def func():
+    """Documentation for a function.
+
+    More details.
+    """
+    pass
+class PyClass:
+    """Documentation for a class.
+
+    More details.
+    """
+
+    def __init__(self):
+        """The constructor."""
+        self.mem_var = 0
+
+    def py_method(self):
+        """Documentation for a method."""
+        pass
+
+    def py_method_2(self):
+        """Documentation for a method."""
+        pass

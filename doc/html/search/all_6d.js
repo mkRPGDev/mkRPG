09e8f3f1ac292f82c827eef04d5ba4361fc2a158
Travis CI <travis@travis-ci.org>
2016-11-17 10:50:04 +0000

    Deploy code docs to GitHub Pages Travis build: 106
    Commit: b4258b38bb84da69f02272f8cf5e99c32dac6928

--- doc/html/search/all_6d.js
+++ doc/html/search/all_6d.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['map',['Map',['../class_map.html',1,'Map'],['../options_8h.html#a4e58380df1435929cc191b5b4ba99cd5',1,'MAP():&#160;options.h']]],
+  ['make_5fgrid',['make_grid',['../classsrc_1_1layer_1_1_layer.html#aa173e4625a648a7c774aa9f6c8fb4e65',1,'src::layer::Layer']]],
   ['map',['Map',['../classsrc_1_1world_1_1_map.html',1,'src::world']]],
+  ['map',['Map',['../class_map.html',1,'Map'],['../options_8h.html#a4e58380df1435929cc191b5b4ba99cd5',1,'MAP():&#160;options.h']]],
   ['map_2eh',['map.h',['../map_8h.html',1,'']]],
   ['map_5fparser',['map_parser',['../namespacesrc_1_1parsing_1_1map__parser.html#a25f5d209c73921d3c5c35b84ef775c5c',1,'src::parsing::map_parser']]],
   ['mapdock',['MapDock',['../class_map_dock.html',1,'']]],
@@ -11,9 +12,9 @@ var searchData=
   ['mapsizechanged',['mapSizeChanged',['../class_map_painter.html#a8f1f85c58564d01e970bfcaf105e5f9b',1,'MapPainter']]],
   ['mapslistmodel',['MapsListModel',['../class_maps_list_model.html',1,'']]],
   ['mapslistmodel_2eh',['mapslistmodel.h',['../mapslistmodel_8h.html',1,'']]],
-  ['mapviewer',['MapViewer',['../class_map_viewer.html',1,'']]],
-  ['mapviewer',['MapViewer',['../classsrc_1_1cursescli_1_1_map_viewer.html',1,'src::cursescli']]],
   ['mapviewer',['MapViewer',['../classsrc_1_1map_1_1_map_viewer.html',1,'src::map']]],
+  ['mapviewer',['MapViewer',['../classsrc_1_1cursescli_1_1_map_viewer.html',1,'src::cursescli']]],
+  ['mapviewer',['MapViewer',['../class_map_viewer.html',1,'']]],
   ['move',['move',['../class_map_painter.html#a55dd52865ad3f50b91dadd1f1b6f9352',1,'MapPainter::move()'],['../classsrc_1_1print_world_1_1_world_viewer.html#a4f40dfbf0e262ffbe723900a2f5cbe4f',1,'src::printWorld::WorldViewer.move()']]],
   ['move_5fchar',['move_char',['../classsrc_1_1print_world_1_1_world_viewer.html#ae16b4b78785490c05644cd54fa2fd88a',1,'src::printWorld::WorldViewer']]]
 ];

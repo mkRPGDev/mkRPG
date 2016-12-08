b23edc5d7e6d1acbf5ca6b82b67e69d0656f96f8
Travis CI <travis@travis-ci.org>
2016-12-08 11:38:25 +0000

    Deploy code docs to GitHub Pages Travis build: 185
    Commit: 2936bc5c9f181cddf6d9da6ad81b9366a6109763

--- doc_py/html/search/classes_63.js
+++ doc_py/html/search/classes_63.js
@@ -3,12 +3,11 @@ var searchData=
   ['cell',['Cell',['../classworld_1_1_cell.html',1,'world']]],
   ['character',['Character',['../classinterface_1_1character_1_1_character.html',1,'interface::character']]],
   ['chat',['Chat',['../classplugins_1_1chat_1_1_chat.html',1,'plugins::chat']]],
-  ['chatgui',['ChatGUI',['../classplugins_1_1chat_1_1_chat_g_u_i.html',1,'plugins::chat']]],
   ['chatui',['ChatUI',['../classplugins_1_1chat_1_1_chat_u_i.html',1,'plugins::chat']]],
   ['chunk',['Chunk',['../classinterface_1_1chunk_1_1_chunk.html',1,'interface::chunk']]],
   ['chunkcache',['ChunkCache',['../classinterface_1_1cache_1_1_chunk_cache.html',1,'interface::cache']]],
-  ['client',['Client',['../classclient_1_1_client.html',1,'client']]],
   ['client',['Client',['../classinterface_1_1pygamecli_1_1_client.html',1,'interface::pygamecli']]],
+  ['client',['Client',['../classclient_1_1_client.html',1,'client']]],
   ['curses',['Curses',['../classinterface_1_1cursescli_1_1_curses.html',1,'interface::cursescli']]],
   ['cursesplugin',['CursesPlugin',['../classplugins_1_1plugin_1_1_curses_plugin.html',1,'plugins::plugin']]]
 ];

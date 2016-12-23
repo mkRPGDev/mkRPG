ab1425ad162cd34858bc7e41997e458ed73312b5
Rémi Oudin <oudin@crans.org>
2016-12-23 12:55:57 +0100

    Handle GUIWindow.
    Creates the class GUIWindow and set default values.

--- src/interface/gui/gui_window.py
+++ src/interface/gui/gui_window.py
@@ -2,12 +2,95 @@
 It relies on the gui_elements module, and the parsing module in order to create
 the view."""
 
-from plugin import PygamePlugin
+from plugins.plugin import PygamePlugin
 from interface.cache import ImageCache
+from interface.const import DEFAULT_GUI_VALUES
+from parsing import gui_parser
+
+
 
 class GUIWindow:
     """ Representation of a gui window xml file """
-    pass
+    def __init__(self, file_path):
+        """ Parses a xml file describing the user interface.
+        It also sets up default values.
+        """
+        data = gui_parser.parse_file(file_path)
+        if data.get('name') != 'Container':
+            # The file doesn't describe a container. Exits the run.
+            print("Invalid file : The first tag isn't a container !")
+            exit(1)
+        self.name = data['name']
+        _params = data.get('params')
+        if _params is None:
+            # Safety check. Shouldn't happen since it should have been checked
+            # In the parsing
+            print("Invalid file : No params for the container.")
+            exit(1)
+        self.params = _params
+        _content = data.get('content')
+        if _content is None:
+            # Safety check. Shouldn't happen since it should have been checked
+            # In the parsing
+            print("Invalid file : No content for the container.")
+            exit(1)
+        self.content = _content
+
+    def _set_defaul_value(self, param):
+        """ Sets the default value for the given param.
+            - ```param``` should be a string."""
+        assert isinstance(param, str)
+        if self.params.get(param) is None:
+            try:
+                value = DEFAULT_GUI_VALUES[param]
+                self.params[param] = value
+            except AttributeError:
+                # Iteratates over general attributes.
+                for possibility in ['color', 'size']:
+                    if possibility in param:
+                        self.params[param] = DEFAULT_GUI_VALUES[possibility]
+                        break
+        try:
+            self.params[param]
+        except AttributeError:
+            raise AttributeError("No default value for parameter %s" % param)
+
+
+    def _set_content_default_values(self, content, param):
+        """ Sets the default value for the given content param.
+        - ```param``` should be a string, content the index of the content in
+        the contents list.
+        """
+        assert isinstance(param, str)
+        assert isinstance(content, int)
+        if self.content[content] is not None\
+            and self.content[content].get(param) is not None:
+            try:
+                value = DEFAULT_GUI_VALUES[param]
+                self.content[content][param] = value
+            except AttributeError:
+                # Iteratates over general attributes.
+                for possibility in ['color', 'size']:
+                    if possibility in param:
+                        self.content[content][param] = DEFAULT_GUI_VALUES[possibility]
+                        break
+        try:
+            self.params[param]
+        except AttributeError:
+            raise AttributeError(
+                "No default value for parameter %s in content %s " %
+                (param, content)
+            )
+
+    def set_default_values(self):
+        """ Sets all default values of the object. """
+        for param in self.params:
+            self._set_defaul_value(param)
+        for (index, _) in enumerate(self.content):
+            for param in self.params:
+                self._set_content_default_values(index, param)
+
+
 
 class GUIWindowView(PygamePlugin):
     """ Viewer of a gui window, pygame client uses instances of this class """

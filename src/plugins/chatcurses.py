b83e4dcc2d6554a1b43d8aeefdb5038b4434ce25
JulesKzl <jules.kozolinsky@ens-cachan.fr>
2017-01-04 23:20:16 +0100

    Review plugins/chatcurses.py, See Issue #14

--- src/plugins/chatcurses.py
+++ src/plugins/chatcurses.py
@@ -1,5 +1,6 @@
-from plugins.plugincurses import CursesPlugin
 import curses
+from plugins.plugincurses import CursesPlugin
+
 
 class ChatView(CursesPlugin):
     MINW = 1000
@@ -21,16 +22,17 @@ class ChatView(CursesPlugin):
 
     def handleKey(self, key):
         """ return True if the key has been used """
-        if key==ord(' ') and not self.writing:
+        if key == ord(' ') and not self.writing:
             self.writing = True
             return True
-        if not self.writing: return False
-        elif key==curses.KEY_BACKSPACE:
+        if not self.writing:
+            return False
+        elif key == curses.KEY_BACKSPACE:
             self.text.pop() if len(self.text) else self.reset()
-        elif key==ord('\n'):#curses.KEY_ENTER:
+        elif key == ord('\n'):#curses.KEY_ENTER:
             self.netPlugin.send(self.text)
             self.reset()
-        elif key==curses.KEY_EXIT:
+        elif key == curses.KEY_EXIT:
             self.reset()
         elif key in range(32, 127):
             self.text.append(key)
@@ -43,15 +45,16 @@ class ChatView(CursesPlugin):
         self.win.erase()
         self.win.border()
         # Anciens messages
-        for k in range(1,self.height-2):
-            if len(self.netPlugin.msgs)<k: break
-            self.win.addstr(self.height-k-2,1,self.netPlugin.msgs[-k])
-        self.win.addch(1,self.width-2,curses.ACS_UARROW)
-        self.win.addch(self.height-3,self.width-2,curses.ACS_DARROW)
+        for k in range(1, self.height-2):
+            if len(self.netPlugin.msgs) < k:
+                break
+            self.win.addstr(self.height-k-2, 1, self.netPlugin.msgs[-k])
+        self.win.addch(1, self.width-2, curses.ACS_UARROW)
+        self.win.addch(self.height-3, self.width-2, curses.ACS_DARROW)
         # Zone d'entrée
         text = self.text[max(0, len(self.text)-self.width+10):]
-        self.win.addstr(self.height-2,1,bytes(text))
+        self.win.addstr(self.height-2, 1, bytes(text))
         if self.writing:
-            self.win.addch(self.height-2,len(text)+1,ord('|'))
+            self.win.addch(self.height-2, len(text)+1, ord('|'))
         else:
-            self.win.addch(self.height-2,len(text)+1,ord('_'))
+            self.win.addch(self.height-2, len(text)+1, ord('_'))

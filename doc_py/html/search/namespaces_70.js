f5251abe1d88c6fd546137a12b782f9bde82afa5
Travis CI <travis@travis-ci.org>
2017-01-05 07:33:14 +0000

    Deploy code docs to GitHub Pages Travis build: 273
    Commit: 2d82c333a7545260a4ced3b2a13f593178ee5994

--- doc_py/html/search/namespaces_70.js
+++ doc_py/html/search/namespaces_70.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['actions_5fparser',['actions_parser',['../namespaceparsing_1_1actions__parser.html',1,'parsing']]],
+  ['chatcurses',['chatcurses',['../namespaceplugins_1_1chatcurses.html',1,'plugins']]],
   ['entity_5fparser',['entity_parser',['../namespaceparsing_1_1entity__parser.html',1,'parsing']]],
   ['global_5fparsing',['global_parsing',['../namespaceparsing_1_1global__parsing.html',1,'parsing']]],
   ['images_5fparser',['images_parser',['../namespaceparsing_1_1images__parser.html',1,'parsing']]],

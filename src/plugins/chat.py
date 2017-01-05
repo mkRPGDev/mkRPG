2381402c810985be4b0129e4cf6a91a29c17e49d
Rémi Oudin <oudin@crans.org>
2017-01-05 08:34:32 +0100

    Closes #13. Docstrings added.

--- src/plugins/chat.py
+++ src/plugins/chat.py
@@ -1,6 +1,13 @@
+"""
+This describes the chat plugin.
+This is only the system that handles the chat and not the module
+that displays the chat on the screen, since it is UI dependent (curses or pygame).
+"""
+
 from plugins.plugin import Plugin
 
 class Chat(Plugin):
+    """ Handles a minimalist chat plugin to be used in the game. """
     MSGID = b"chat"
 
     def __init__(self, e):
@@ -8,9 +15,13 @@ class Chat(Plugin):
         self.msgs = ["Connected"]
 
     def serverMessage(self, msg):
+        """ Broadcasts a message on whole clients. """
         self.broadcast(msg)
 
     def clientMessage(self, msg):
+        """ Receives a message and appends it to the messages to be displayed.
+        Then, it repaints the screen in order to update the view of the chat.
+        """
         self.msgs.append(msg)
         if self.ui:
             self.ui.repaint()
